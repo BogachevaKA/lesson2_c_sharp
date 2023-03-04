bb69a66c3cc70343e5b60432c0afc4ded6a9bba5
BogachevaKA1 <BogachevaKA1@it.mos.ru>
2023-03-04 21:19:51 +0300

    commit-3 for task-1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num < 100 & num > 999)
+if (num >= 100 & num <= 999)
+{
+    Console.WriteLine($"Второе число: {num / 10 % 10}");
+}
+//int x1 = num / 10 % 10;
+else
 {
     Console.WriteLine("Введено не трёхзначное число");
-    return;
 }
-int x1 = num / 10 % 10;
-Console.WriteLine($"Второе число: {x1}");
